9aaf4def4d9e2ae02515e2140a6fe5735e61486f
Olof Rydh <te16.olof.rydh@umea.nti.se>
2017-12-08 15:19:11 +0100

    .

---
+++ anslagstavla/action_page.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <!--head>
+    <meta charset="utf-8">
+    <title>Anslagstavla</title>
+    <link rel="stylesheet" href="style.css">
+  </head-->
+  <body>
+    
+    hello <?php echo $_POST["arende"]; ?> <br>
+    
+  </body>
+</html>
